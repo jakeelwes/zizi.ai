93a143ee305b33edcc780cd4f9b5c36afa5a271a
Jake Elwes <me@jakeelwes.com>
2020-07-01 01:36:08 +0100

    hide youtube

---
+++ tests/youtube.js
@@ -0,0 +1,57 @@
+     var tag = document.createElement('script');
+
+     tag.src = "https://www.youtube.com/iframe_api";
+     var firstScriptTag = document.getElementsByTagName('script')[0];
+     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+     // 3. This function creates an <iframe> (and YouTube player)
+     //    after the API code downloads.
+     var player;
+     var videotime = 0;
+     function onYouTubeIframeAPIReady() {
+       player = new YT.Player('player', {
+         height: '390',
+         width: '640',
+         videoId: 'dlEAM2EQci4',
+         events: {
+           'onReady': onPlayerReady,
+           'onStateChange': onPlayerStateChange
+         }
+       });
+     }
+
+     // 4. The API will call this function when the video player is ready.
+     function onPlayerReady(event) {
+       //event.target.playVideo();
+       document.getElementById('play').innerHTML = '<a href="#" onclick="play();">Play (from hidden YouTube)</a>';
+       function updateTime() {
+         var oldTime = videotime;
+         if(player && player.getCurrentTime) {
+           videotime = player.getCurrentTime();
+
+           console.log(videotime);
+           document.getElementById("time").innerHTML = videotime.toFixed(2);
+         }
+       }
+       timeupdater = setInterval(updateTime, 40);
+       
+       play();
+     }
+
+     function play(){
+       player.playVideo();
+     }
+
+     // 5. The API calls this function when the player's state changes.
+     //    The function indicates that when playing a video (state=1),
+     //    the player should play for six seconds and then stop.
+     var done = false;
+     function onPlayerStateChange(event) {
+       // if (event.data == YT.PlayerState.PLAYING && !done) {
+       //   setTimeout(stopVideo, 6000);
+       //   done = true;
+       // }
+     }
+     function stopVideo() {
+       player.stopVideo();
+     }
