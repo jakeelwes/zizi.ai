6489ffad7b3e5b102044478e8782ab5134b6b1a2
Jake Elwes <me@jakeelwes.com>
2020-10-07 20:05:24 +0100

    build2

---
+++ build/precache-manifest.14222e2f27ae62e47c78c9c4f54cd2a4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80e8f7275c8054a8e052412ffe739b09",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "0ebd577f8ba143409236",
+    "url": "/build/static/css/main.833230a3.chunk.css"
+  },
+  {
+    "revision": "4ab717bf55fcb3f64c3b",
+    "url": "/build/static/js/2.abe76e9c.chunk.js"
+  },
+  {
+    "revision": "b82ecbe7a0ff62182021f97974ada2d8",
+    "url": "/build/static/js/2.abe76e9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ebd577f8ba143409236",
+    "url": "/build/static/js/main.708f9287.chunk.js"
+  },
+  {
+    "revision": "8657584e31b9694b405e",
+    "url": "/build/static/js/runtime-main.a951175a.js"
+  },
+  {
+    "revision": "d55dffe26cb6adf3d14d6ebbb870bd23",
+    "url": "/build/static/media/market_deco-webfont.d55dffe2.woff2"
+  },
+  {
+    "revision": "f40c2500b5ae6058d41f1cf9f3d84cfb",
+    "url": "/build/static/media/market_deco-webfont.f40c2500.woff"
+  },
+  {
+    "revision": "b40a88ee48d4e3d4573f334de719484c",
+    "url": "/build/static/media/market_deco.b40a88ee.ttf"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bab92aca13bd253f03dcb21bc07905b6.js"
+  "/build/precache-manifest.14222e2f27ae62e47c78c9c4f54cd2a4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
