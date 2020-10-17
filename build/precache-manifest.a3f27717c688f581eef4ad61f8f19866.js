ecbaee2532fe5a069fc584c96b46d647699a1aad
Jake Elwes <me@jakeelwes.com>
2020-10-17 05:17:29 +0100

    build BETA

---
+++ build/precache-manifest.a3f27717c688f581eef4ad61f8f19866.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "825b2affab5d5138b5eb98b970fa883d",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "e5fb79b290777352c777",
+    "url": "/build/static/css/main.7341ba59.chunk.css"
+  },
+  {
+    "revision": "3319fff0c79a5ff302ac",
+    "url": "/build/static/js/2.6fa21ad4.chunk.js"
+  },
+  {
+    "revision": "b82ecbe7a0ff62182021f97974ada2d8",
+    "url": "/build/static/js/2.6fa21ad4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5fb79b290777352c777",
+    "url": "/build/static/js/main.bf01a359.chunk.js"
+  },
+  {
+    "revision": "8657584e31b9694b405e",
+    "url": "/build/static/js/runtime-main.a951175a.js"
+  },
+  {
+    "revision": "d55dffe26cb6adf3d14d6ebbb870bd23",
+    "url": "/build/static/media/market_deco-webfont.d55dffe2.woff2"
+  },
+  {
+    "revision": "f40c2500b5ae6058d41f1cf9f3d84cfb",
+    "url": "/build/static/media/market_deco-webfont.f40c2500.woff"
+  },
+  {
+    "revision": "b40a88ee48d4e3d4573f334de719484c",
+    "url": "/build/static/media/market_deco.b40a88ee.ttf"
+  }
+]);
