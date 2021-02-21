a67fd01fb4d11d27a302994d320ef0d1066b0bd7
Jake Elwes <me@jakeelwes.com>
2021-02-21 22:17:18 +0000

    update test

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.51a64e0cd5488d8781f601803caf5df5.js"
+  "./precache-manifest.49af4356d2dc366186ca7a2c501463f2.js"
 );
 
 self.addEventListener('message', (event) => {
